69e9081d9805faf6ef80120357ddf25f44bc6dae
localuser <localuser@VEA.LV>
2023-05-18 10:19:14 +0300

    new proffesor class and modified mom.xml file

---
+++ src/main/java/lv/venta/models/Professor.java
@@ -0,0 +1,66 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "professor_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Professor {
+	
+	//TODO uzlikt Data JPA anotācijas
+	//TODO uzlikt atbilstošās validāciju anotācijas
+	//TODO izveidot Student klasi
+	//TODO izveidot Course klasi
+	//TODO izveidot Grade klasi
+	
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idp")//DB puse būs kolonna "idp" un būs kā auto increment PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idp;
+	
+	@Column(name = "Name")//DB puse būs kolonna title
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burtam jābūt lielajam")
+	@Size(min = 3, max = 15, message = "Jabūt vismaz 3 un ne vairāk kā 15 simboliem")
+	private String name;
+	
+	@Column(name = "Surname")//DB puse būs kolonna title
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burtam jābūt lielajam")
+	@Size(min = 3, max = 15, message = "Jabūt vismaz 3 un ne vairāk kā 15 simboliem")
+	private String surname;
+	
+	@Column(name = "Degree")//DB puse būs kolonna title
+	@NotNull
+	private Degree degree;
+
+	public Professor(String name, String surname, Degree degree) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.degree = degree;
+	}
+	
+	
+	
+	
+	
+	
+}
