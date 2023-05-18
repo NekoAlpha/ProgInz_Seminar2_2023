14eef87a59baf9df18835d518a392c6937a44083
localuser <localuser@VEA.LV>
2023-05-18 10:47:49 +0300

    new almost completed course class

--- src/main/java/lv/venta/models/Course.java
+++ src/main/java/lv/venta/models/Course.java
@@ -1,7 +1,15 @@
 package lv.venta.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +23,19 @@ import lombok.ToString;
 @ToString
 public class Course {
 	
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idc")//DB puse būs kolonna "idp" un būs kā auto increment PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idc;
+	
+	@Column(name = "Title")//DB puse būs kolonna title
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burtam jābūt lielajam")
+	@Size(min = 5, max = 25, message = "Jabūt vismaz 5 un ne vairāk kā 25 simboliem")
+	private String title;
+	
+	private int creditPoints;
 	
 	
 	
