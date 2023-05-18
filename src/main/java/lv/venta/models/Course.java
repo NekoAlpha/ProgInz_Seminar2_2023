aba63d7354740c1414cbc47cfb51a134dcc6a03e
localuser <localuser@VEA.LV>
2023-05-18 11:00:38 +0300

    new: link between course and professor

--- src/main/java/lv/venta/models/Course.java
+++ src/main/java/lv/venta/models/Course.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -42,6 +44,9 @@ public class Course {
 	@Max(value = 20)
 	private int creditPoints;
 	
+	@OneToOne
+	@JoinColumn(name = "Idp")//sasaistam ar otras klases PK
+	private Professor professor;
 	
 	//TODO izveidot argumenta konstruktoru
 	
--- src/main/java/lv/venta/models/Professor.java
+++ src/main/java/lv/venta/models/Professor.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -52,6 +53,9 @@ public class Professor {
 	@NotBlank
 	private Degree degree;
 
+	@OneToOne(mappedBy = "professor")//sasaistam ar otras klases mainīgo
+	private Course course;
+	
 	public Professor(String name, String surname, Degree degree) {
 		super();
 		this.name = name;
