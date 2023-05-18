c438e440d84528f22eb72527b503b0012648fdd3
localuser <localuser@VEA.LV>
2023-05-18 11:45:24 +0300

    Add: model in main class

--- src/main/java/lv/venta/ProgInzSeminar220231Application.java
+++ src/main/java/lv/venta/ProgInzSeminar220231Application.java
@@ -1,7 +1,19 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.models.Course;
+import lv.venta.models.Degree;
+import lv.venta.models.Grade;
+import lv.venta.models.Professor;
+import lv.venta.models.Student;
+import lv.venta.repos.ICourseRepo;
+import lv.venta.repos.IGradeRepo;
+import lv.venta.repos.IProfessorRepo;
+import lv.venta.repos.IStudentRepo;
 
 @SpringBootApplication
 public class ProgInzSeminar220231Application {
@@ -9,5 +21,41 @@ public class ProgInzSeminar220231Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgInzSeminar220231Application.class, args);
 	}
+	
+	@Bean//izsauks funkciju automātiski, kad startēsies sistēma
+	public CommandLineRunner testModel(IProfessorRepo profRepo, IStudentRepo stRepo, ICourseRepo courRepo, IGradeRepo grRepo) {
+		
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				
+				Professor pr1 = new Professor("Karina", "Skirmante", Degree.mg);
+				Professor pr2 = new Professor("Martins", "Saulitis", Degree.mg);
+				profRepo.save(pr1);
+				profRepo.save(pr2);
+				
+				Student st1 = new Student("Renārs", "Geriksons");
+				Student st2 = new Student("Janis", "Berzins");
+				stRepo.save(st1);
+				stRepo.save(st2);
+				
+				Course c1 = new Course("JAVA I", 4, pr1);
+				Course c2 = new Course("Datubazes", 4, pr2);
+				courRepo.save(c1);
+				courRepo.save(c2);
+				
+				grRepo.save(new Grade(10, st1, c1));//Janis nopelnija 10 JAVA
+				grRepo.save(new Grade(6, st1, c2));//Janis nopelnija 10 Datubazes
+				grRepo.save(new Grade(8, st2, c1));
+				grRepo.save(new Grade(10, st2, c2));
+
+				
+			}
+		};
+		
+	}
+	
+	
 
 }
