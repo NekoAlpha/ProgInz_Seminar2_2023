57a1298c9450d254abb510dfd87c0ff75a6702cd
localuser <localuser@VEA.LV>
2023-05-18 11:14:41 +0300

    new: link between course and grade

--- src/main/java/lv/venta/models/Grade.java
+++ src/main/java/lv/venta/models/Grade.java
@@ -39,6 +39,9 @@ public class Grade {
 	@JoinColumn(name = "Ids")
 	private Student student;
 	
+	@ManyToOne
+	@JoinColumn(name = "Idc")
+	private Course course;
 	
 	
 	//TODO izveidot argumenta konstruktoru
