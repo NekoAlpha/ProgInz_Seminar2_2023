e0e246df2988e973fd29defffc6240b64b6c1d72
localuser <localuser@VEA.LV>
2023-05-19 09:47:30 +0300

    Edit: change TestModel to many to many

--- src/main/java/lv/venta/ProgInzSeminar220231Application.java
+++ src/main/java/lv/venta/ProgInzSeminar220231Application.java
@@ -1,5 +1,9 @@
 package lv.venta;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -32,18 +36,36 @@ public class ProgInzSeminar220231Application {
 				
 				Professor pr1 = new Professor("Karina", "Skirmante", Degree.mg);
 				Professor pr2 = new Professor("Martins", "Saulitis", Degree.mg);
+				Professor pr3 = new Professor("Raita", "Rollande", Degree.phd);
+				//TODO izveidot velvienu profesoru
 				profRepo.save(pr1);
 				profRepo.save(pr2);
+				profRepo.save(pr3);
 				
 				Student st1 = new Student("Renārs", "Geriksons");
 				Student st2 = new Student("Janis", "Berzins");
 				stRepo.save(st1);
 				stRepo.save(st2);
 				
-				Course c1 = new Course("Java i", 4, pr1);
-				Course c2 = new Course("Datubazes", 4, pr2);
+				//TODO uztaisīt kursu, kuram ir divi profesori piesaistīti
+				//TODO uztaisīt gadījumu, kad viens profesors pasbiedz divus kursus
+				Course c1 = new Course("Proginz", 4, new ArrayList<>(Arrays.asList(pr1, pr3)));
+				Course c2 = new Course("Datubazes", 4, new ArrayList<>(Arrays.asList(pr2)));
+				Course c3 = new Course("Datu struktūras", 2, new ArrayList<>(Arrays.asList(pr1)));
 				courRepo.save(c1);
 				courRepo.save(c2);
+				courRepo.save(c3);
+				
+				c1.addProfessor(pr1);
+				c1.addProfessor(pr3);
+				c2.addProfessor(pr2);
+				c3.addProfessor(pr1);
+				courRepo.save(c1);
+				courRepo.save(c2);
+				courRepo.save(c3);
+				
+				
+				
 				
 				grRepo.save(new Grade(10, st1, c1));//Janis nopelnija 10 JAVA
 				grRepo.save(new Grade(6, st1, c2));//Janis nopelnija 10 Datubazes
