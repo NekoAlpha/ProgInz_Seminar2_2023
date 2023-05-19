cb0e0e5d24cfd2eb5cdbd896a78e7047ddf5e3d1
localuser <localuser@VEA.LV>
2023-05-19 10:32:57 +0300

    Add: added some filter functions

--- src/main/java/lv/venta/repos/IGradeRepo.java
+++ src/main/java/lv/venta/repos/IGradeRepo.java
@@ -1,9 +1,18 @@
 package lv.venta.repos;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.models.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Long>{
-
+	//TODO atlasīt visu studentu atzīmes, ja ir zināms studenta id
+	ArrayList<Grade> findByStudentIdst(long idst);
+	
+	//TODO atlasīt visa kursa atzīmes, ja ir zināms kursa nosaukums
+	ArrayList<Grade> findByCourseTitle(String title);
+	
+	
+	
 }
